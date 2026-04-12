5ba4d4d41a470b6cd438c1497eb13d32e501094e
Raul <72282208+RaulPellegrini@users.noreply.github.com>
2026-04-12 21:55:43 +0200

    Movements

--- Assets/Scripts/PLayerController.cs
+++ Assets/Scripts/PLayerController.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
 
-public class PLayerController : MonoBehaviour
-{
+
+public class PlayerController : MonoBehaviour
+{ 
+    [SerializeField] private float speed = 5f;
+    private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+         rb = GetComponent<Rigidbody>();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        Vector3 direction = Vector3.zero; //new Vector3(0,0,0);
+
+        if(Input.GetKey(KeyCode.W)) direction += Vector3.forward;            
+        if(Input.GetKey(KeyCode.S)) direction += Vector3.back;
+        if(Input.GetKey(KeyCode.A)) direction += Vector3.left;
+        if(Input.GetKey(KeyCode.D)) direction += Vector3.right;
+        if(Input.GetKey(KeyCode.Space)) direction += Vector3.up;
+
+        direction = direction.normalized;
+        rb.MovePosition(rb.position + direction *speed * Time.fixedDeltaTime);
+        
+        }
+
+    void OnCollisionEnter(Collision collision)
     {
         
     }
 }
+
